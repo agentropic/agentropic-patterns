97efdd19f0dd82810cef905d8bac03b9abf7821d
daniel <kaluifesinachi@yahoo.com>
2026-01-13 18:49:13 +0100

    Holarchic organization patterns

---
+++ src/holarchy/mod.rs
@@ -0,0 +1,9 @@
+﻿//! Holarchic organization patterns
+
+/// Holarchy structure
+pub mod holarchy;
+/// Holon (autonomous unit)
+pub mod holon;
+
+pub use holarchy::Holarchy;
+pub use holon::{Holon, HolonType};
